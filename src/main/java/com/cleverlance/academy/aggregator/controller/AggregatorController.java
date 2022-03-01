2b2baad3f6e13a48fe51eb6e66fe314b78d43482
pavel <pavel_moravec@hotmail.com>
2022-03-01 17:21:00 +0100

    Prubezna integrace s Tomasem - origin

--- src/main/java/com/cleverlance/academy/aggregator/controller/AggregatorController.java
+++ src/main/java/com/cleverlance/academy/aggregator/controller/AggregatorController.java
@@ -3,17 +3,34 @@ package com.cleverlance.academy.aggregator.controller;
 import com.cleverlance.academy.aggregator.model.Address;
 import com.cleverlance.academy.aggregator.model.Identification;
 import com.cleverlance.academy.aggregator.model.Person;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class AggregatorController {
-
-    @GetMapping(path = "/identification")
-    public ResponseEntity<Identification> getIdentification() {
-        return ResponseEntity.ok(new Identification("Moje meteostanice",
-                new Address("Danci", "2556/6D", "Ricany"), new Person("Tomas", "Michalicka")));
+    
+    private final List<Identification> identifications = new ArrayList<>();
+    
+    @PostMapping("/server")
+    public ResponseEntity<Void> saveServer(@RequestBody Identification identification) {
+        log.info("Server identification: {}", identification);
+        
+        this.identifications.add(identification);
+        
+        return ResponseEntity.ok().build();
     }
-
+    
+    @GetMapping("/server")
+    public ResponseEntity<List<Identification>> getIdentifications() {
+        
+        return ResponseEntity.ok(this.identifications);
+    }
+    
 }
--- src/main/java/com/cleverlance/academy/aggregator/model/Identification.java
+++ src/main/java/com/cleverlance/academy/aggregator/model/Identification.java
@@ -4,9 +4,9 @@ import lombok.Value;
 
 @Value
 public class Identification {
-
+    
     private String name;
     private Address address;
     private Person owner;
-
+    
 }
